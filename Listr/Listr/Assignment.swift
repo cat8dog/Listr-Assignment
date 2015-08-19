bd526cab72e25132a3fca4daafa11455800b24ed
cat8dog <catherine.reyto@gmail.com>
2015-08-19 17:46:34 -0400

    Part 1 / Constraints complete

---
+++ Listr/Listr/Assignment.swift
@@ -0,0 +1,24 @@
+//Once the project is created, open the Main.storyboard in Interface Builder and you should find a square view controller with a default 600×600 canvas.
+//
+//Drag a UIImageView from the Object Library to the view controller. Set its width to 600 and height to 390. Choose the image view and go to Attributes inspector. Set the image to “tshirt” and the mode to Aspect Fill.
+//
+//Drag a UIView to the ViewController and put it right below the UIImageView. This view serves as a container for holding other UI components like labels. By grouping related UI components under the same view, it will be easier for you to work with auto layout in a later section.
+//
+//In Size inspector, make sure you set the width to 600 and height to 210. We will refer to the UIView as the Product Info View from now on.
+//
+//Drag a label to the Product Info View. Change the label to “BitMaker Labs Logo.” Set the label's font to Helvetica Neue and it's font size to 32 points. In Size inspector, change the value of X to 22 and Y to 15. Set the width to 556 and height to 44.
+//
+//Drag another label and place it right below the previous label. In Attributes inspector, change the text to This is logo presented by @bitmakerlabs. The PSD comes with a plain simple logo mockup template. You can edit the logo colour and use the smart layer to apply your designs. The high-resolution makes it easy to frame specific details with close-ups.
+//
+//Change this label's font to Helvetica Neue, the font size to 18 points and the number of lines to 0.
+//
+//Under Size inspector, change the value of X to 22 and Y to 58. Set the width to 556 and height to 123.
+//
+//In Xcode 6, you are no longer required to run the app in a simulator to see how the view appears. The latest version of Xcode comes with a preview assistant which allows developers evaluate the resulting design on different size displays. In Interface Builder, open the Assistant pop-up menu > Preview (1).
+//
+//Then press and hold shift+option key, and click Main.storyboard (Preview).
+//
+//You’ll be prompted to choose where to display the preview. Double click the + button next to the main area. Xcode will then display a preview of the app’s UI in the assistant editor. By default, it shows you the preview on an iPhone 4-inch device.
+
+
+import Foundation
