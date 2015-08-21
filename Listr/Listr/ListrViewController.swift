0d5296e390776c31027ffef2e8f06db2d1c5bcb5
cat8dog <catherine.reyto@gmail.com>
2015-08-20 22:06:41 -0400

    Alert segue works

--- Listr/Listr/ListrViewController.swift
+++ Listr/Listr/ListrViewController.swift
@@ -13,54 +13,53 @@ class ListrViewController: UIViewController, UITableViewDelegate, UITableViewDat
     var myPeeps = ["Cat", "Gavin", "Hudson", "Dennis", "CZ", "Elise"]
     @IBOutlet weak var tableView: UITableView!
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+//        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
-    
    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.myPeeps.count
     }
     
-    
-    // added delegate method for didSelectRowAtIndexPath
-    func  tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        let alertController = UIAlertController(title: "Row Selected", message: "You've selected a row", preferredStyle: .Alert)
-        
-        let okayAction = UIAlertAction(title: "OKAY", style: .Cancel) {
-            (action) in
-            println(action)
-        }
-        
-      //  presentViewController(alertController, animated: true)
-    }
+
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
         cell.textLabel?.text = self.myPeeps[indexPath.row]
         return cell
     }
-
     
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    
+    
+    // added delegate method for didSelectRowAtIndexPath
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        if segue.identifier == "presentation" {
-            if let destinationVC = segue.destinationViewController as? ViewController {
-                println("this tutorial really blows!!!")
-            }
+    
+    // alert variable (feature of UIAlertController class, as the former UIAlertView is now depreciated)
+        var alert:UIAlertController = UIAlertController(title: "Row Selected", message: "You've selected a row", preferredStyle: UIAlertControllerStyle.Alert)
+    
+    // alert action variable that will perform the segue action when clicked.
+        var defaultAction:UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (action) -> Void in
+        
+        self.performSegueWithIdentifier("alertSegue", sender: self)
+        
         }
-        
+     
+    // attaches the action ("OK" button) as a property of the alert variable.
+        alert.addAction(defaultAction)
+        self.presentViewController(alert, animated: true) { () -> Void in
+            
+        }
+  
     }
-
+    
 
 }
-
-
-
-
-
